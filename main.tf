3916d5c024a4d165ef075c379bbbce92378338b0
Sagar Urade <centosop@gmail.com>
2025-01-03 15:36:16 +0530

    work of 3jan25

---
+++ main.tf
@@ -0,0 +1,35 @@
+resource "aws_vpc" "base" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "TF VPC"
+  }
+}
+
+resource "aws_subnet" "web2" {
+  vpc_id            = aws_vpc.base.id
+  cidr_block        = "10.100.0.0/24"
+  availability_zone = "ap-south-1b"
+  tags = {
+    Name = "web-2"
+  }
+  depends_on = [aws_vpc.base]
+}
+resource "aws_subnet" "db1" {
+  vpc_id            = aws_vpc.base.id
+  availability_zone = "ap-south-1a"
+  cidr_block        = "10.100.1.0/24"
+  tags = {
+    Name = "db-1"
+  }
+  depends_on = [aws_vpc.base]
+}
+resource "aws_subnet" "db2" {
+  vpc_id            = aws_vpc.base.id
+  cidr_block        = "10.100.2.0/24"
+  availability_zone = "ap-south-1b"
+  tags = {
+    Name = "db-2"
+  }
+  depends_on = [aws_vpc.base]
+}
+
