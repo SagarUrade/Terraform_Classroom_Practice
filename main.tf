bff37b50042b5c2ff58e90beacd0cb6509ddbde8
Sagar Urade <centosop@gmail.com>
2025-01-10 17:54:16 +0530

    wethout variables on 10jan25

--- main.tf
+++ main.tf
@@ -5,9 +5,18 @@ resource "aws_vpc" "base" {
   }
 }
 
-resource "aws_subnet" "web2" {
+resource "aws_subnet" "web1" {
   vpc_id            = aws_vpc.base.id
   cidr_block        = "10.100.0.0/24"
+  availability_zone = "ap-south-1b"
+  tags = {
+    Name = "web-1"
+  }
+  depends_on = [aws_vpc.base]
+}
+resource "aws_subnet" "web2" {
+  vpc_id            = aws_vpc.base.id
+  cidr_block        = "10.100.1.0/24"
   availability_zone = "ap-south-1b"
   tags = {
     Name = "web-2"
@@ -17,7 +26,7 @@ resource "aws_subnet" "web2" {
 resource "aws_subnet" "db1" {
   vpc_id            = aws_vpc.base.id
   availability_zone = "ap-south-1a"
-  cidr_block        = "10.100.1.0/24"
+  cidr_block        = "10.100.3.0/24"
   tags = {
     Name = "db-1"
   }
@@ -25,7 +34,7 @@ resource "aws_subnet" "db1" {
 }
 resource "aws_subnet" "db2" {
   vpc_id            = aws_vpc.base.id
-  cidr_block        = "10.100.2.0/24"
+  cidr_block        = "10.100.4.0/24"
   availability_zone = "ap-south-1b"
   tags = {
     Name = "db-2"
