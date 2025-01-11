f61ebc7bc9a99a4547482799ec56574e57105cb4
Sagar Urade <94967140+SagarUrade@users.noreply.github.com>
2025-01-11 16:00:55 +0530

    Added Variable 11jan25
    Completed variable

--- variables.tf
+++ variables.tf
@@ -1,10 +1,44 @@
 variable "vpc_name" {
-  type = string
-  default = "All"
-  description = "vpc name"
+  description = "The name of the VPC"
+  type        = string
+  default     = "TF VPC"
 }
 
-variable "subnet_name" {
-  type = list(string)
-  default = [ "web1" ]
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
+
+variable "subnet_cidrs" {
+  description = "A list of CIDR blocks for the subnets"
+  type        = list(string)
+  default = [
+    "10.0.0.0/24", # Subnet for web1
+    "10.0.1.0/24", # Subnet for web2
+    "10.0.2.0/24", # Subnet for db1
+    "10.0.3.0/24"  # Subnet for db2
+  ]
+}
+
+variable "subnet_names" {
+  description = "A list of names for the subnets"
+  type        = list(string)
+  default = [
+    "Application-1",
+    "Application-2",
+    "Database-1",
+    "database-2"
+  ]
+}
+
+variable "availability_zones" {
+  description = "A list of availability zones to use for subnets"
+  type        = list(string)
+  default = [
+    "ap-south-1a",
+    "ap-south-1b"
+  ]
+}
+
+
