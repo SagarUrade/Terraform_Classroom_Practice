90edecd728f3b314d98b77afe7b2e53321e4f951
Sagar Urade <94967140+SagarUrade@users.noreply.github.com>
2025-01-11 16:02:06 +0530

    vairable updated 11jan25

--- main.tf
+++ main.tf
@@ -1,44 +1,17 @@
 resource "aws_vpc" "base" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
   tags = {
-    Name = "TF VPC"
+    Name = var.vpc_name
   }
 }
 
 resource "aws_subnet" "web1" {
   vpc_id            = aws_vpc.base.id
-  cidr_block        = "10.100.0.0/24"
-  availability_zone = "ap-south-1b"
+  count             = 4
+  cidr_block        = element(var.subnet_cidrs, count.index)
+  availability_zone = element(var.availability_zones, count.index)
   tags = {
-    Name = "web-1"
+    Name = element(var.subnet_names, count.index)
   }
   depends_on = [aws_vpc.base]
 }
-resource "aws_subnet" "web2" {
-  vpc_id            = aws_vpc.base.id
-  cidr_block        = "10.100.1.0/24"
-  availability_zone = "ap-south-1b"
-  tags = {
-    Name = "web-2"
-  }
-  depends_on = [aws_vpc.base]
-}
-resource "aws_subnet" "db1" {
-  vpc_id            = aws_vpc.base.id
-  availability_zone = "ap-south-1a"
-  cidr_block        = "10.100.3.0/24"
-  tags = {
-    Name = "db-1"
-  }
-  depends_on = [aws_vpc.base]
-}
-resource "aws_subnet" "db2" {
-  vpc_id            = aws_vpc.base.id
-  cidr_block        = "10.100.4.0/24"
-  availability_zone = "ap-south-1b"
-  tags = {
-    Name = "db-2"
-  }
-  depends_on = [aws_vpc.base]
-}
-
